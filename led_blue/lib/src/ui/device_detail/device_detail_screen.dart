1bdf2ba192fbabdbea81163e14448eb075ada9f3
Samet Sarıal <sarialsamet@gmail.com>
2023-02-05 23:34:34 +0300

    initial commit

--- led_blue/lib/src/ui/device_detail/device_detail_screen.dart
+++ led_blue/lib/src/ui/device_detail/device_detail_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:led_blue/src/ble/ble_device_connector.dart';
 import 'package:led_blue/src/ui/device_detail/device_log_tab.dart';
+import 'package:led_blue/src/ui/device_detail/timer/timer_screen.dart';
 import 'package:provider/provider.dart';
 
 import 'device_interaction_tab.dart';
@@ -36,7 +37,7 @@ class _DeviceDetail extends StatelessWidget {
           return true;
         },
         child: DefaultTabController(
-          length: 2,
+          length: 3,
           child: Scaffold(
             appBar: AppBar(
               actions: [],
@@ -51,6 +52,7 @@ class _DeviceDetail extends StatelessWidget {
                   device: device,
                 ),
                 const DeviceLogTab(),
+                TimerScreenTab(device: device)
               ],
             ),
           ),
@@ -58,32 +60,36 @@ class _DeviceDetail extends StatelessWidget {
       );
 
   Widget tabBarNav() {
-    return TabBar(
-      labelColor: Colors.white,
-      unselectedLabelColor: Colors.white,
-      indicatorSize: TabBarIndicatorSize.tab,
-      indicator: BoxDecoration(
-          gradient:
-              LinearGradient(colors: [Color(0xFF2F80ED), Color(0xFF2F80ED)]),
-          borderRadius: BorderRadius.circular(50),
-          color: Colors.black),
-      tabs: [
-        Tab(
-          text: 'FARBE',
-        ),
-        Tab(
-          text: 'MODI',
-        ),
-        Tab(
-          text: 'TÖNE',
-        ),
-        Tab(
-          text: 'MIC',
-        ),
-        Tab(
-          text: 'TIMER',
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TabBar(
+        labelColor: Colors.white,
+        unselectedLabelColor: Colors.white,
+        indicatorPadding: EdgeInsets.all(5),
+        indicator: BoxDecoration(
+            gradient:
+                LinearGradient(colors: [Color(0xFF2F80ED), Color(0xFF2F80ED)]),
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.black),
+        indicatorSize: TabBarIndicatorSize.tab,
+        tabs: [
+          Tab(
+            text: 'FARBE',
+          ),
+          Tab(
+            text: 'MODI',
+          ),
+          Tab(
+            text: 'TÖNE',
+          ),
+          Tab(
+            text: 'MIC',
+          ),
+          Tab(
+            text: 'TIMER',
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ led_blue/lib/src/ui/device_detail/timer/timer_screen.dart
@@ -0,0 +1,530 @@
+import 'dart:ffi';
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:functional_data/functional_data.dart';
+import 'package:led_blue/src/ble/ble_device_connector.dart';
+import 'package:led_blue/src/ble/ble_device_interactor.dart';
+import 'package:led_blue/src/ui/device_detail/characteristic_interaction_dialog.dart';
+import 'package:led_blue/src/ui/device_detail/device_interaction_tab.dart';
+import 'package:provider/provider.dart';
+
+part 'timer_screen.g.dart';
+//ignore_for_file: annotate_overrides
+
+class TimerScreenTab extends StatelessWidget {
+  final DiscoveredDevice device;
+
+  const TimerScreenTab({
+    required this.device,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) =>
+      Consumer3<BleDeviceConnector, ConnectionStateUpdate, BleDeviceInteractor>(
+        builder: (context, deviceConnector, connectionStateUpdate,
+                serviceDiscoverer, __) =>
+            _TimerScreen(
+          viewModel: TimerScreenViewModel(
+              deviceId: device.id,
+              connectionStatus: connectionStateUpdate.connectionState,
+              deviceConnector: deviceConnector,
+              service: serviceDiscoverer,
+              discoverServices: () =>
+                  serviceDiscoverer.discoverServices(device.id)),
+        ),
+      );
+}
+
+@immutable
+@FunctionalData()
+class TimerScreenViewModel extends $TimerScreenViewModel {
+  const TimerScreenViewModel({
+    required this.deviceId,
+    required this.connectionStatus,
+    required this.deviceConnector,
+    required this.discoverServices,
+    required this.service,
+  });
+
+  final String deviceId;
+  final DeviceConnectionState connectionStatus;
+  final BleDeviceConnector deviceConnector;
+  final BleDeviceInteractor service;
+
+  @CustomEquality(Ignore())
+  final Future<List<DiscoveredService>> Function() discoverServices;
+
+  bool get deviceConnected =>
+      connectionStatus == DeviceConnectionState.connected;
+
+  Future<void> connect() async {
+    print('connected');
+    await deviceConnector.connect(deviceId);
+  }
+
+  void disconnect() {
+    deviceConnector.disconnect(deviceId);
+  }
+}
+
+class _TimerScreen extends StatefulWidget {
+  const _TimerScreen({
+    required this.viewModel,
+    Key? key,
+  }) : super(key: key);
+
+  final TimerScreenViewModel viewModel;
+
+  @override
+  _TimerScreenState createState() => _TimerScreenState();
+}
+
+class _TimerScreenState extends State<_TimerScreen> {
+  List<DiscoveredService> discoveredServices = [];
+  bool isOn = false;
+  double _brightness = 0.0;
+  String openTime = 'MO';
+  String closeTime = 'MO';
+  bool openRunning = false;
+  bool closeRunning = false;
+
+  @override
+  void initState() {
+    try {
+      initilize();
+    } catch (e) {
+      print(e);
+    }
+
+    super.initState();
+  }
+
+  initilize() async {
+    await widget.viewModel.connect().then((value) async {
+      //discoverServices();
+    });
+  }
+
+  Future<List<DiscoveredService>> discoverServices() async {
+    final result = await widget.viewModel.discoverServices();
+
+    setState(() {
+      discoveredServices = result;
+    });
+    return result;
+  }
+
+  ledOff() async {
+    print('led off');
+
+    await writeToDevice(
+            [126, 4, 4, 0, 0, 0, 256, 0, 239, 51, 51, 51, 51, 51, 51, 51])
+        .then((value) {
+      if (value) {
+        print('led off success');
+        setState(() {
+          isOn = false;
+        });
+      } else {
+        print('led off failed');
+        setState(() {
+          isOn = true;
+        });
+      }
+    });
+  }
+
+  ledOn() async {
+    print('led on');
+
+    await writeToDevice(
+            [126, 4, 4, 240, 0, 0, 256, 0, 239, 51, 51, 51, 51, 51, 51, 51])
+        .then((value) {
+      if (value) {
+        print('led on success');
+        setState(() {
+          isOn = true;
+        });
+      } else {
+        print('led on failed');
+        setState(() {
+          isOn = false;
+        });
+      }
+    });
+  }
+
+  Color pickerColor = Color(0xff219653);
+  Color currentColor = Color(0xff219653);
+
+  //add type
+  List<Map<String, dynamic>> days = [
+    {'day': 'MO', 'openSelected': false, 'closeSelected': false, 'value': 129},
+    {'day': 'DI', 'openSelected': false, 'closeSelected': false, 'value': 2},
+    {'day': 'MI', 'openSelected': false, 'closeSelected': false, 'value': 4},
+    {'day': 'DO', 'openSelected': false, 'closeSelected': false, 'value': 8},
+    {'day': 'FR', 'openSelected': false, 'closeSelected': false, 'value': 16},
+    {'day': 'SA', 'openSelected': false, 'closeSelected': false, 'value': 32},
+    {'day': 'SO', 'openSelected': false, 'closeSelected': false, 'value': 64},
+  ];
+
+  Future<bool> writeToDevice(List<int> deviceCode) async {
+    print('CHANGE STARTED');
+    try {
+      List<DiscoveredService> data = await discoverServices();
+      if (discoveredServices.length > 0 && discoveredServices != null) {
+        for (var i = 0; i < discoveredServices.length; i++) {
+          for (var j = 0;
+              j < discoveredServices[i].characteristics.length;
+              j++) {
+            //check uuid of characteristic and write value
+            DiscoveredCharacteristic characteristic =
+                discoveredServices[i].characteristics[j];
+            if (characteristic.characteristicId.toString().contains('fff3')) {
+              print(characteristic.characteristicId.toString());
+              //write value to characteristic with id
+              QualifiedCharacteristic data = QualifiedCharacteristic(
+                  serviceId: discoveredServices[i].serviceId,
+                  characteristicId: characteristic.characteristicId,
+                  deviceId: widget.viewModel.deviceId);
+              try {
+                await widget.viewModel.service
+                    .writeCharacterisiticWithoutResponse(data, deviceCode);
+                setState(() {
+                  isOn = false;
+                });
+                return true;
+              } catch (e) {
+                return false;
+              }
+            }
+          }
+        }
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+// ValueChanged<Color> callback
+  void changeColor(Color color) async {
+    setState(() => pickerColor = color);
+    if (isOn) {
+      print('COLOR CHANGE');
+      writeToDevice([
+        126,
+        7,
+        5,
+        3,
+        pickerColor.red,
+        pickerColor.green,
+        pickerColor.blue,
+        0,
+        239,
+        51,
+        51,
+        51,
+        51,
+        51,
+        51,
+        51
+      ]);
+    } else {
+      print('colorchange33');
+      ledOn();
+      writeToDevice([
+        126,
+        7,
+        5,
+        3,
+        pickerColor.red,
+        pickerColor.green,
+        pickerColor.blue,
+        0,
+        239,
+        51,
+        51,
+        51,
+        51,
+        51,
+        51,
+        51
+      ]);
+    }
+  }
+
+  changeBrightness() {
+    if (isOn) {
+      writeToDevice([
+        126,
+        4,
+        1,
+        _brightness.toInt(),
+        1,
+        255,
+        255,
+        0,
+        239,
+        33,
+        33,
+        33,
+        33,
+        33,
+        33,
+        33
+      ]);
+    } else {
+      print('BRIGHTNESS change33');
+      ledOn();
+      writeToDevice([
+        126,
+        4,
+        1,
+        _brightness.toInt(),
+        1,
+        255,
+        255,
+        0,
+        239,
+        33,
+        33,
+        33,
+        33,
+        33,
+        33,
+        33
+      ]);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverList(
+          delegate: SliverChildListDelegate.fixed(
+            [
+              Padding(
+                padding:
+                    const EdgeInsetsDirectional.only(top: 8.0, bottom: 16.0),
+                child: Text(
+                  "Timer",
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Color(0xffF3F3F3),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.punch_clock),
+                          SizedBox(width: 10),
+                          Text(
+                            "Automatisch anschalten",
+                            style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                        ],
+                      ),
+                      CupertinoSwitch(
+                        value: openRunning,
+                        onChanged: (value) {
+                          setState(() {
+                            openRunning = value;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 20),
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(children: [
+                        //listview builder for colors in favColors list
+                        for (var i = 0; i < days.length; i++)
+                          GestureDetector(
+                            onTap: () {
+                              if (!openRunning) {
+                                if (days[i]['openSelected']) {
+                                  setState(() {
+                                    days[i]['openSelected'] = false;
+                                  });
+                                } else {
+                                  setState(() {
+                                    days[i]['openSelected'] = true;
+                                  });
+                                }
+                              }
+                            },
+                            child: Container(
+                              margin:
+                                  const EdgeInsets.symmetric(horizontal: 5.0),
+                              decoration: BoxDecoration(
+                                color: days[i]['openSelected']
+                                    ? Colors.blue
+                                    : Colors.white,
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              width: 35.0,
+                              height: 35.0,
+                              child: Center(
+                                  child: new Text(days[i]['day'],
+                                      style: TextStyle(
+                                          color: days[i]['openSelected']
+                                              ? Colors.white
+                                              : Colors.black))),
+                            ),
+                          )
+                      ]),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Uhrzeit",
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                      Text(
+                        "00:00",
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                    ],
+                  ),
+                ]),
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Color(0xffF3F3F3),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.punch_clock),
+                          SizedBox(width: 10),
+                          Text(
+                            "Automatisch ausschalten",
+                            style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                        ],
+                      ),
+                      CupertinoSwitch(
+                        value: closeRunning,
+                        onChanged: (value) {
+                          setState(() {
+                            closeRunning = value;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 20),
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(children: [
+                        //listview builder for colors in favColors list
+                        for (var i = 0; i < days.length; i++)
+                          GestureDetector(
+                            onTap: () {
+                              if (!closeRunning) {
+                                if (days[i]['closeSelected']) {
+                                  setState(() {
+                                    days[i]['closeSelected'] = false;
+                                  });
+                                } else {
+                                  setState(() {
+                                    days[i]['closeSelected'] = true;
+                                  });
+                                }
+                              }
+                            },
+                            child: Container(
+                              margin:
+                                  const EdgeInsets.symmetric(horizontal: 5.0),
+                              decoration: BoxDecoration(
+                                color: days[i]['closeSelected']
+                                    ? Colors.blue
+                                    : Colors.white,
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              width: 35.0,
+                              height: 35.0,
+                              child: Center(
+                                  child: new Text(days[i]['day'],
+                                      style: TextStyle(
+                                          color: days[i]['closeSelected']
+                                              ? Colors.white
+                                              : Colors.black))),
+                            ),
+                          )
+                      ]),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Uhrzeit",
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                      Text(
+                        "00:00",
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                    ],
+                  ),
+                ]),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
